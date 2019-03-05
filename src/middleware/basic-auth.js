86971bce035146ce95b6779fec6a35dcd9a2e4f5
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-05 12:33:11 -0500

    Initial commit
    - started from https://github.com/thinkful-ei-armadillo/michael-ethan-thinkful-server

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+
+function requireAuth(req, res, next) {
+
+  const authToken = req.get('Authorization') || ''
+
+  let bearerToken
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' })
+  } else {
+    bearerToken = authToken.slice(7, authToken.length)
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer
+    .from(bearerToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  req.app.get('db')('thingful_users')
+    .where({ user_name: tokenUserName })
+    .first()
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+      }
+
+      return bcrypt.compare(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request' })
+          }
+
+          req.user = user;
+          next();
+        })
+        .catch(next);
+    });
+}
+
+module.exports = {
+  requireAuth,
+};
